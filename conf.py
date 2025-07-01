c3f9a31322f5b93e2e0ef919fb68632a96962aa7
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2025-06-30 23:12:54 -0400

    Renamed Saw Dust (removed 'Codex')

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "The Cloistered Monkey"  # (translatable)
-BLOG_TITLE = "Codex Saw Dust"  # (translatable)
+BLOG_TITLE = "SAW Dust"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://necromuralist.github.io/Codex-Saw-Dust/"
